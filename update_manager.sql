40f2f99189d18c6f5b0582b33998f410f8403c94
mcerruti0311 <mcerruti@sis.us>
2018-02-16 10:58:14 -0800

    rev 3

--- update_manager.sql
+++ update_manager.sql
@@ -1,5 +1,9 @@
-Select Id, Name, Employee_Number__c, ReportsTo.Name, ReportsTo.Employee_Number__c
+SELECT Id,
+  Employee_Number__c,
+  Name,
+  ReportsTo.Employee_Number__c,
+  ReportsTo.Name
 FROM Contact
 WHERE ReportsTo.Name = 'Nicole Mogannam'
-AND Salesforce_User__c != null
-AND Salesforce_User__r.IsActive = true
+  AND Salesforce_User__c != null
+  AND Salesforce_User__r.IsActive = true
